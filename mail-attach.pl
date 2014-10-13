041003d5565d2af7ec5ece4ec0db905b7fabf6ec
wq1 <wq1@users.noreply.github.com>
2014-10-14 08:08:04 +0900

    mail-attach.pl: Add main function

--- mail-attach.pl
+++ mail-attach.pl
@@ -1,45 +1,82 @@
-#!/usr/bin/perl
+#! /usr/bin/perl
 
 use strict;
 use warnings;
 use utf8;
+use Encode;
+use open (
+  IN  => ':encoding(utf8)',
+  OUT => ':utf8',
+  ':std'
+);
+@ARGV = map { decode('utf8', $_) } @ARGV;
 
-use open IN  => ':encoding(utf8)';
-use open OUT => ':utf8';
-use open ':std';
+use MIME::Base64 ('encode_base64');
 
-use Encode       qw(encode_utf8);
-use MIME::Base64 qw(encode_base64);
+my($utf8, $cp932, $locale);
+$utf8   = find_encoding('utf8');
+$cp932  = find_encoding('cp932');
+$locale = $utf8;
 
-my($to, $from, $file, $name, $sub, $fn, $fh, $buf, $bs, $body,
-   $contentType, $contentDisposition);
 
-$to   = 'foo@example.com';
-$from = 'bar@example.com';
-$file = $ARGV[0];
-$name = qx(ls '$file' | xargs -I{} basename {}); chomp($name);
+sub main() {
 
-$fn   = '=?UTF-8?B?'.
-        encode_base64(encode_utf8($name), '').
-        '?=';
-$sub  = $fn;
+  my(
+    $to, $from, $sub, $body,
+    $contentType, $contentDisposition,
+    $file, $name, $fn, $fh, $buf, $bs, $cmd
+  );
 
-$contentType  = qx(file -b --mime-type '$file'); chomp($contentType);
-$contentType .= "; name=\"$fn\"";
-$contentDisposition = "attachment; filename=\"$fn\"";
+  $to   = 'foo@example.com';
+  $from = 'bar@example.com';
 
-open($fh, '<:raw', $file) or die "$!:$file";
-$bs = '4096';
-while (read($fh, $buf, $bs)) {
-  $body .= $buf;
+  $file = $ARGV[0];
+  $name = _qx("ls '$file' | xargs -I{} basename {}");
+  $fn   = '=?UTF-8?B?'.
+          encode_base64(encode_utf8($name), '').
+          '?=';
+  $sub  = $fn;
+
+  $contentType  = _qx("file -b --mime-type '$file'");
+  $contentType .= "; name=\"$fn\"";
+  $contentDisposition = "attachment; filename=\"$fn\"";
+
+  open($fh, '<:raw', $file) or die "$!:$file";
+  $bs = '4096';
+  while (read($fh, $buf, $bs)) {
+    $body .= $buf;
+  }
+  close($fh);
+  $body = encode_base64($body);
+
+  $cmd = "mail".
+         " -aContent-Type:'$contentType'".
+         " -aContent-Disposition:'$contentDisposition'".
+         " -aContent-Transfer-Encoding:'base64'".
+         " -s $sub".
+         " -aFrom:$from $to";
+  open($fh, '|-:raw', $cmd) or die "$!";
+  print($fh $body);
+  close($fh);
+
+  return 0;
+  
+} exit(main());
+
+
+sub _qx($) {
+
+  my($cmd, $rtn);
+  $cmd = _encode($locale, $_[0]);
+  $rtn = qx($cmd);
+  chomp($rtn);
+  return($rtn);
+  
 }
-close($fh);
 
-$body = encode_base64($body); chomp($body);
 
-open($fh, '|-:raw', "mail -aContent-Type:'$contentType' -aContent-Disposition:'$contentDisposition' -aContent-Transfer-Encoding:'base64' -s $sub -aFrom:$from $to")
- or die "$!";
-print($fh $body);
-close($fh);
+sub _encode($$) {
 
-exit;
+  return($_[0]->encode($_[1]));
+  
+}
